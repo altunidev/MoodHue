acfcae08a93f514e84ead30374ebd01001252ab8
Salty <salty@altuni.dev>
2025-02-26 02:58:14 -0600

    Added a ton more face params based on unrecognized values. Note: may need pruning.

--- processor.py
+++ processor.py
@@ -22,6 +22,12 @@ def process_data(queue, debug_level=1, throttle_ms=1000):
         "eyeRightX": {"value": 0, "alt_names": ["EyeRightX"]},
         "eyeLidLeft": {"value": 0, "alt_names": ["EyeLidLeft"]},
         "eyeLidRight": {"value": 0, "alt_names": ["EyeLidRight"]},
+
+        # Additional Eye Squint parameters
+        "EyeSquintLeft1": {"value": 0, "alt_names": ["EyeSquintLeft1"]},
+        "EyeSquintRight1": {"value": 0, "alt_names": ["EyeSquintRight1"]},
+        "EyeSquintLeft2": {"value": 0, "alt_names": ["EyeSquintLeft2"]},
+        "EyeSquintRight2": {"value": 0, "alt_names": ["EyeSquintRight2"]},
         
         # Eyebrow parameters
         "browDownLeft": {"value": 0, "alt_names": ["BrowDownLeft", "LeftBrowDown", "BrowExpressionLeft"]},
@@ -34,6 +40,15 @@ def process_data(queue, debug_level=1, throttle_ms=1000):
         "browPinchRight2": {"value": 0, "alt_names": ["BrowPinchRight2"]},
         "browInnerUp1": {"value": 0, "alt_names": ["BrowInnerUp1"]},
         "browInnerUp2": {"value": 0, "alt_names": ["BrowInnerUp2"]},
+        "BrowPinchLeft4": {"value": 0, "alt_names": ["BrowPinchLeft4"]},
+        "BrowPinchRight4": {"value": 0, "alt_names": ["BrowPinchRight4"]},
+        "BrowInnerUp4": {"value": 0, "alt_names": ["BrowInnerUp4"]},
+        "BrowExpressionLeft1": {"value": 0, "alt_names": ["BrowExpressionLeft1"]},
+        "BrowExpressionRight1": {"value": 0, "alt_names": ["BrowExpressionRight1"]},
+        "BrowExpressionLeft2": {"value": 0, "alt_names": ["BrowExpressionLeft2"]},
+        "BrowExpressionRight2": {"value": 0, "alt_names": ["BrowExpressionRight2"]},
+        "BrowExpressionLeft4": {"value": 0, "alt_names": ["BrowExpressionLeft4"]},
+        "BrowExpressionRight4": {"value": 0, "alt_names": ["BrowExpressionRight4"]},
         
         # Mouth parameters
         "mouthOpen": {"value": 0, "alt_names": ["MouthOpen", "JawOpen", "JawDrop"]},
@@ -42,6 +57,12 @@ def process_data(queue, debug_level=1, throttle_ms=1000):
         "mouthFrown": {"value": 0, "alt_names": ["MouthFrown", "Frown", "FrownLeft", "FrownRight", "MouthCornerDepressor"]},
         "mouthPucker": {"value": 0, "alt_names": ["MouthPucker", "Pucker", "LipPucker"]},
         "mouthClosed": {"value": 0, "alt_names": ["MouthClosed"]},
+        "MouthClosed1": {"value": 0, "alt_names": ["MouthClosed1"]},
+        "MouthClosed2": {"value": 0, "alt_names": ["MouthClosed2"]},
+        "MouthClosed4": {"value": 0, "alt_names": ["MouthClosed4"]},
+        "MouthClosed8": {"value": 0, "alt_names": ["MouthClosed8"]},
+        "MouthDimple1": {"value": 0, "alt_names": ["MouthDimple1"]},
+        "MouthDimple2": {"value": 0, "alt_names": ["MouthDimple2"]},
         "mouthShrugUpper": {"value": 0, "alt_names": ["MouthShrugUpper", "UpperLipRaise"]},
         "mouthShrugLower": {"value": 0, "alt_names": ["MouthShrugLower", "LowerLipDepress"]},
         "mouthUpperUp1": {"value": 0, "alt_names": ["MouthUpperUp1"]},
@@ -50,7 +71,61 @@ def process_data(queue, debug_level=1, throttle_ms=1000):
         "mouthLowerDown2": {"value": 0, "alt_names": ["MouthLowerDown2"]},
         "mouthPress1": {"value": 0, "alt_names": ["MouthPress1"]},
         "mouthPress2": {"value": 0, "alt_names": ["MouthPress2"]},
+        
+        # Mouth Raiser parameters
+        "MouthRaiserLower1": {"value": 0, "alt_names": ["MouthRaiserLower1"]},
+        "MouthRaiserLower2": {"value": 0, "alt_names": ["MouthRaiserLower2"]},
+        "MouthRaiserLower4": {"value": 0, "alt_names": ["MouthRaiserLower4"]},
+        "MouthRaiserUpper1": {"value": 0, "alt_names": ["MouthRaiserUpper1"]},
+        "MouthRaiserUpper2": {"value": 0, "alt_names": ["MouthRaiserUpper2"]},
+        "MouthUpperUp4": {"value": 0, "alt_names": ["MouthUpperUp4"]},
+        "MouthUpperUp8": {"value": 0, "alt_names": ["MouthUpperUp8"]},
+        "MouthLowerDown4": {"value": 0, "alt_names": ["MouthLowerDown4"]},
+
+        # Mouth position parameters
+        "MouthX1": {"value": 0, "alt_names": ["MouthX1"]},
+        "MouthX2": {"value": 0, "alt_names": ["MouthX2"]},
+        "MouthX4": {"value": 0, "alt_names": ["MouthX4"]},
+        "MouthX8": {"value": 0, "alt_names": ["MouthX8"]},
+        "MouthXNegative": {"value": 0, "alt_names": ["MouthXNegative"]},
+
+        # Jaw parameters
+        "JawZ1": {"value": 0, "alt_names": ["JawZ1"]},
+        "JawZ2": {"value": 0, "alt_names": ["JawZ2"]},
+        "JawZ4": {"value": 0, "alt_names": ["JawZ4"]},
+        "JawX1": {"value": 0, "alt_names": ["JawX1"]},
+        "JawX2": {"value": 0, "alt_names": ["JawX2"]},
+        "JawX4": {"value": 0, "alt_names": ["JawX4"]},
+        "JawXNegative": {"value": 0, "alt_names": ["JawXNegative"]},
+        "JawForward1": {"value": 0, "alt_names": ["JawForward1"]},
+        "JawForward2": {"value": 0, "alt_names": ["JawForward2"]},
+        "JawForward4": {"value": 0, "alt_names": ["JawForward4"]},
+        "JawOpen1": {"value": 0, "alt_names": ["JawOpen1"]},
+        "JawOpen2": {"value": 0, "alt_names": ["JawOpen2"]},
+        "JawOpen4": {"value": 0, "alt_names": ["JawOpen4"]},
+        "JawOpen8": {"value": 0, "alt_names": ["JawOpen8"]},
+        "JawOpen16": {"value": 0, "alt_names": ["JawOpen16"]},
+        
+        # Lip parameters
+        "LipSuckLower1": {"value": 0, "alt_names": ["LipSuckLower1"]},
+        "LipSuckLower2": {"value": 0, "alt_names": ["LipSuckLower2"]},
+        "LipSuckLower4": {"value": 0, "alt_names": ["LipSuckLower4"]},
+        "LipSuckUpper1": {"value": 0, "alt_names": ["LipSuckUpper1"]},
+        "LipSuckUpper2": {"value": 0, "alt_names": ["LipSuckUpper2"]},
+        "LipSuckUpper4": {"value": 0, "alt_names": ["LipSuckUpper4"]},
+        "LipPucker1": {"value": 0, "alt_names": ["LipPucker1"]},
+        "LipPucker2": {"value": 0, "alt_names": ["LipPucker2"]},
+        "LipPucker4": {"value": 0, "alt_names": ["LipPucker4"]},
+        "LipFunnel1": {"value": 0, "alt_names": ["LipFunnel1"]},
+        "LipFunnel2": {"value": 0, "alt_names": ["LipFunnel2"]},
+        "LipFunnel4": {"value": 0, "alt_names": ["LipFunnel4"]},
+
+        # Tongue parameters
         "tongueOut": {"value": 0, "alt_names": ["TongueOut"]},
+        "TongueOut1": {"value": 0, "alt_names": ["TongueOut1"]},
+        "TongueOut2": {"value": 0, "alt_names": ["TongueOut2"]},
+        "TongueOut4": {"value": 0, "alt_names": ["TongueOut4"]},
+        "TongueOut8": {"value": 0, "alt_names": ["TongueOut8"]},
         
         # Cheek parameters
         "cheekPuff": {"value": 0, "alt_names": ["CheekPuff", "CheekBlow"]},
@@ -58,12 +133,48 @@ def process_data(queue, debug_level=1, throttle_ms=1000):
         "cheekSquintRight": {"value": 0, "alt_names": ["CheekSquintRight", "RightCheekSquint"]},
         "cheekPuffSuckLeft1": {"value": 0, "alt_names": ["CheekPuffSuckLeft1"]},
         "cheekPuffSuckRight1": {"value": 0, "alt_names": ["CheekPuffSuckRight1"]},
+        "CheekPuffSuckLeft2": {"value": 0, "alt_names": ["CheekPuffSuckLeft2"]},
+        "CheekPuffSuckRight2": {"value": 0, "alt_names": ["CheekPuffSuckRight2"]},
+        "CheekPuffSuckLeft4": {"value": 0, "alt_names": ["CheekPuffSuckLeft4"]},
+        "CheekPuffSuckRight4": {"value": 0, "alt_names": ["CheekPuffSuckRight4"]},
+        "CheekSquint1": {"value": 0, "alt_names": ["CheekSquint1"]},
+        "CheekSquint2": {"value": 0, "alt_names": ["CheekSquint2"]},
+        "CheekSquint4": {"value": 0, "alt_names": ["CheekSquint4"]},
+
+        # Smile/Sad parameters with intensity levels
+        "SmileSadLeft1": {"value": 0, "alt_names": ["SmileSadLeft1"]},
+        "SmileSadRight1": {"value": 0, "alt_names": ["SmileSadRight1"]},
+        "SmileSadLeft2": {"value": 0, "alt_names": ["SmileSadLeft2"]},
+        "SmileSadRight2": {"value": 0, "alt_names": ["SmileSadRight2"]},
+        "SmileSadLeft4": {"value": 0, "alt_names": ["SmileSadLeft4"]},
+        "SmileSadRight4": {"value": 0, "alt_names": ["SmileSadRight4"]},
+        "SmileSadLeft8": {"value": 0, "alt_names": ["SmileSadLeft8"]},
+        "SmileSadRight8": {"value": 0, "alt_names": ["SmileSadRight8"]},
+        "SmileSadLeftNegative": {"value": 0, "alt_names": ["SmileSadLeftNegative"]},
+        "SmileSadRightNegative": {"value": 0, "alt_names": ["SmileSadRightNegative"]},
+        
+        # Smile/Frown parameters
+        "SmileFrownLeft1": {"value": 0, "alt_names": ["SmileFrownLeft1"]},
+        "SmileFrownRight1": {"value": 0, "alt_names": ["SmileFrownRight1"]},
+        "SmileFrownLeft2": {"value": 0, "alt_names": ["SmileFrownLeft2"]},
+        "SmileFrownRight2": {"value": 0, "alt_names": ["SmileFrownRight2"]},
+        "SmileFrownLeft4": {"value": 0, "alt_names": ["SmileFrownLeft4"]},
+        "SmileFrownRight4": {"value": 0, "alt_names": ["SmileFrownRight4"]},
+        "SmileFrownLeft8": {"value": 0, "alt_names": ["SmileFrownLeft8"]},
+        "SmileFrownRight8": {"value": 0, "alt_names": ["SmileFrownRight8"]},
         
         # Nose parameters
         "noseSneerLeft": {"value": 0, "alt_names": ["NoseSneerLeft", "LeftNoseSneer", "NoseWrinkleLeft"]},
         "noseSneerRight": {"value": 0, "alt_names": ["NoseSneerRight", "RightNoseSneer", "NoseWrinkleRight"]},
         "noseSneer1": {"value": 0, "alt_names": ["NoseSneer1"]},
-        "noseSneer2": {"value": 0, "alt_names": ["NoseSneer2"]}
+        "noseSneer2": {"value": 0, "alt_names": ["NoseSneer2"]},
+        "NoseSneer4": {"value": 0, "alt_names": ["NoseSneer4"]},
+        "NoseSneerLeft1": {"value": 0, "alt_names": ["NoseSneerLeft1"]},
+        "NoseSneerRight1": {"value": 0, "alt_names": ["NoseSneerRight1"]},
+        "NoseSneerLeft2": {"value": 0, "alt_names": ["NoseSneerLeft2"]},
+        "NoseSneerRight2": {"value": 0, "alt_names": ["NoseSneerRight2"]},
+        "NoseSneerLeft4": {"value": 0, "alt_names": ["NoseSneerLeft4"]},
+        "NoseSneerRight4": {"value": 0, "alt_names": ["NoseSneerRight4"]}
     }
     
     # Create a mapping from all possible names to standard parameter names
